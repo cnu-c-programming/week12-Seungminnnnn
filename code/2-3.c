283f78898cf110abdcc91606da6c377e8e20702c
Seungminnnnn <114681724+Seungminnnnn@users.noreply.github.com>
2026-05-28 23:19:45 +0900

    Update 2-3.c

--- code/2-3.c
+++ code/2-3.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
 void logger(char* message) {
-    FILE* fp = fopen("log.txt", "w");
+    FILE* fp = fopen("log.txt", "a");
+
+    if (fp == NULL) {
+        return;
+    }
+
     fprintf(fp, "%s\n", message);
+    fclose(fp);
 }
 
 int job() {
