e6b32116affe88c44b6d8d55f7cf007f35a5c53d
Seungminnnnn <114681724+Seungminnnnn@users.noreply.github.com>
2026-05-28 23:12:53 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 
 int main(int argc, const char* argv[]) {
-    if (argc < 2)
+    if (argc < 2) {
         return 0;
+    }
 
     FILE* fp = fopen(argv[1], "r");
 
+    FILE* fp1 = fopen("myfile.txt","w");
 
+    int count[26] = {0};
+    char alpha;
+
+    while(fscanf(fp,"%c", &alpha) == 1) {
+        if (alpha >= 'a' && alpha <= 'z') {
+            count[alpha - 'a']++;
+        }
+    }
+
+    for (int i = 0; i < 26; i++) {
+        fprintf(fp1, "%c: %d\n", 'a'+i, count[i]);
+    }
 
     fclose(fp);
-}
+    fclose(fp1);
 
+    return 0;
+}
