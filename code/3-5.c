260d993f6e48b4f4f3089b1ad459504f4dfb66c6
Seungminnnnn <114681724+Seungminnnnn@users.noreply.github.com>
2026-05-28 23:28:32 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -4,15 +4,34 @@ int main(int argc, const char* argv[]) {
     const char* filename = "numbers.txt";
 
     FILE* fp = fopen(filename, "r");
+
     if (fp == NULL) {
         return 0;
     }
 
-
     int sum = 0;
+    int num;
 
+    char invalid[100];
+
+    while (1) {
+        int result = fscanf(fp, "%d", &num);
+
+        if (result == 1) {
+            sum += num;
+        }
+        else if (result == EOF) {
+            break;
+        }
+        else {
+            fscanf(fp, "%s", invalid);
+            fprintf(stderr, "invalid input %s\n", invalid);
+        }
+    }
 
     printf("sum: %d\n", sum);
+
     fclose(fp);
+
     return 0;
 }
