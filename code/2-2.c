c7cf8f71bb6d4dd712143dd6e9d90023b10b22d2
Seungminnnnn <114681724+Seungminnnnn@users.noreply.github.com>
2026-05-28 23:11:56 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -8,15 +8,29 @@ typedef struct student {
 
 int main(int argc, const char* argv[]) {
     FILE* fp = fopen("student.txt", "r");
+
     int count = 0;
-    Student students[64];
+    Student student[64];
 
-
+    while (fscanf(fp, "%s %d", student[count].name, &student[count].score) == 2) {
+        count++;
+    }
 
     int max = 0;
     float avg = 0;
+    int sum = 0;
 
+    for (int i = 0; i < count; i++) {
+        if (student[i].score > max) {
+            max = student[i].score;
+        }
 
+        sum += student[i].score;
+    }
+
+    if (count > 0) {
+        avg = (float)sum / count;
+    }
 
     printf("max: %d\n", max);
     printf("avg: %.2f\n", avg);
@@ -25,4 +39,3 @@ int main(int argc, const char* argv[]) {
 
     return 0;
 }
-
