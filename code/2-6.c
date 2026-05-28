e0f84057231535a0fdf71d07c0eb4eb3731841f5
Seungminnnnn <114681724+Seungminnnnn@users.noreply.github.com>
2026-05-28 23:13:25 +0900

    Update 2-6.c

--- code/2-6.c
+++ code/2-6.c
@@ -2,14 +2,20 @@
 #include <string.h>
 
 int main(int argc, const char* argv[]) {
-    if (argc < 3)
+    if (argc < 3) {
         return 0;
+    }
 
     FILE* fp = fopen(argv[1], "r");
     const char* target_str = argv[2];
 
+    char line[1024];
 
-    
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        if (strstr(line, target_str) != NULL) {
+            printf("%s", line);
+        }
+    }
+
     fclose(fp);
 }
-
