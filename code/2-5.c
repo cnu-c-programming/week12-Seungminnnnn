974d33fb4a8330040f6c44ef9746bff2a659d902
Seungminnnnn <114681724+Seungminnnnn@users.noreply.github.com>
2026-05-28 23:13:09 +0900

    Update 2-5.c

--- code/2-5.c
+++ code/2-5.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 
 int main(int argc, const char* argv[]) {
-    if (argc < 3)
+    if (argc < 3) {
         return 0;
+    }
 
+    FILE* fp_src = fopen(argv[1], "r");
+    FILE* fp_dst = fopen(argv[2], "w");
 
+    char ch;
+    while (fscanf(fp_src,"%c", &ch) == 1) {
+        fprintf(fp_dst,"%c",ch);
+    }
+
+    fclose(fp_src);
+    fclose(fp_dst);
+
+    return 0;
 }
-
